4f96a89a8b70aa6de64800ba70988cbcb9754a56
Charles_Magid <cmmagid@gmail.com>
2010-03-07 00:53:23 -0500

    added credits

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :credits
+
   map.resources :contact_pages
 
   map.resources :home_pages
@@ -36,6 +38,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+
+  map.root :controller => "home_pages"
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100307035710) do
+ActiveRecord::Schema.define(:version => 20100307052724) do
 
   create_table "contact_pages", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "credits", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
